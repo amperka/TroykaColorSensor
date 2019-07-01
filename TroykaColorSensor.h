864c0f57c50cb0e46eb35d105133238103d72e94
Yury Botov <yury.botov@gmail.com>
2019-07-01 10:46:26 +0300

    Add "envelope" header TroykaColorSensor.h

---
+++ TroykaColorSensor.h
@@ -0,0 +1,21 @@
+/*
+ * This file is a part of Troyka modules library.
+ *
+ * Defines: Troyka color sensor module library. Uses Adafruit_TCS34725 library.
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Yury Botov <by@amperka.ru>
+ * License: GPLv3, all text here must be included in any redistribution.
+ */
+
+#ifndef __TROYKACOLORSENSOR_H__
+#define __TROYKACOLORSENSOR_H__
+
+#include "Adafruit_TCS34725.h"
+
+// Adafruit_TCS34725.h and Adafruit_TCS34725.cpp was modifyed.
+// Modifyed parts is ranged by //__TROYKACOLORSENSOR_H__ start/end blocks
+
+typedef Adafruit_TCS34725 TroykaColorSensor;
+
+#endif // __TROYKAPROXIMITY_H__
